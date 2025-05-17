05aed63f99fa5937c91ea3156a0dd009172e2cf7
Ju5t1nL3 <justinle2006@gmail.com>
2025-05-17 16:19:44 -0500

    fixed spotify with new mac update

--- .config/sketchybar/items/media.sh
+++ .config/sketchybar/items/media.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sketchybar --add event spotify_change "com.spotify.client.PlaybackStateChanged"
+
 sketchybar --add item media e \
             --set media label.max_chars=30 \
                         icon.padding_left=0 \
@@ -7,4 +9,4 @@ sketchybar --add item media e \
                         icon=􀑪             \
                         background.drawing=off \
                         script="$PLUGIN_DIR/media.sh" \
-            --subscribe media media_change
+            --subscribe media spotify_change
--- .config/sketchybar/plugins/media.sh
+++ .config/sketchybar/plugins/media.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-STATE="$(echo "$INFO" | jq -r '.state')"
-if [ "$STATE" = "playing" ]; then
-    MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
-    sketchybar --set $NAME label="$MEDIA" drawing=on
-else
-    sketchybar --set $NAME drawing=off
+# 1) Is Spotify running?
+if ! pgrep -x Spotify >/dev/null; then
+  sketchybar --set $NAME drawing=off
+  exit
 fi
+
+# 2) Get player state
+STATE=$(osascript -e 'tell application "Spotify" to player state')
+if [[ "$STATE" != "playing" ]]; then
+  sketchybar --set $NAME drawing=off
+  exit
+fi
+
+# 3) Get track info
+TITLE=$(osascript -e 'tell application "Spotify" to name of current track')
+ARTIST=$(osascript -e 'tell application "Spotify" to artist of current track')
+MEDIA="$TITLE — $ARTIST"
+
+# 4) Update the bar
+sketchybar --set $NAME \
+  label="$MEDIA" \
+  drawing=on
