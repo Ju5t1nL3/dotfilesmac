2d23737ee777cce339184049e157967a7973c046
Ju5t1nL3 <justinle2006@gmail.com>
2025-08-23 19:17:58 -0500

    Center front app below the notch
    Upgraded to a new mac with a notch, so now the curr front app
    started to become blocked. So, I moved it below the notch.

--- .config/sketchybar/items/front_app.sh
+++ .config/sketchybar/items/front_app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sketchybar --add item front_app left \
+sketchybar --add item front_app center \
             --set front_app icon.font="sketchybar-app-font:Regular:16.0" \
                             script="$PLUGIN_DIR/front_app.sh"            \
+                            y_offset=-15 \
             --subscribe front_app front_app_switched
