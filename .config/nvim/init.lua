697c931a3ca16040477bdf3c34caa8fc127ca42b
Ju5t1nL3 <justinle2006@gmail.com>
2025-10-23 17:21:06 -0500

    Add common lsps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -351,19 +351,24 @@ require("lazy").setup({
 			--  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			local servers = {
-				-- clangd = {},
-				-- gopls = {},
-				-- pyright = {},
-				-- rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--
-				-- Some languages (like typescript) have entire language plugins that can be useful:
-				--    https://github.com/pmizio/typescript-tools.nvim
-				--
-				-- But for many setups, the LSP (`ts_ls`) will work just fine
-				-- ts_ls = {},
-				--
+				-- Python
+				ruff = {},
+				pyrefly = {},
 
+				-- Web Dev
+				emmet_language_server = {},
+				html = {},
+				cssls = {},
+				vtsls = {},
+				tailwindcss = {},
+				astro = {},
+
+				-- Other
+				clangd = {}, -- C, C++, Objective-C
+				jdtls = {}, -- Java
+				texlab = {}, -- Latex
+
+				-- General Purpose
 				lua_ls = {
 					-- cmd = { ... },
 					-- filetypes = { ... },
@@ -378,6 +383,10 @@ require("lazy").setup({
 						},
 					},
 				},
+
+				harper_ls = {
+					filetypes = { "markdown", "html", "typst" },
+				}, -- Grammar checking
 			}
 
 			-- To check the current status of installed tools and/or manually install
