72095f31f9b73237d507b483b415bf2da86d2c38
Ju5t1nL3 <justinle2006@gmail.com>
2025-10-23 22:04:26 -0500

    Configure kickstart plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -616,11 +616,9 @@ require("lazy").setup({
 	},
 
 	-- require 'kickstart.plugins.debug',
-	-- require 'kickstart.plugins.indent_line',
-	-- require 'kickstart.plugins.lint',
-	-- require 'kickstart.plugins.autopairs',
+	require("kickstart.plugins.indent_line"),
+	require("kickstart.plugins.autopairs"),
 	require("kickstart.plugins.neo-tree"),
-	-- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
 	-- { import = 'custom.plugins' },
 })
