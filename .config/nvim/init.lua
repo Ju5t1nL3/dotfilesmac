01c1c00c80931c6381c7c4bbef3335eff1d4b5ef
Ju5t1nL3 <justinle2006@gmail.com>
2025-10-25 16:44:50 -0500

    Stop formatting on save

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -405,10 +405,10 @@ require("lazy").setup({
 		},
 		opts = {
 			notify_on_error = false,
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
+			-- format_on_save = {
+			-- 	timeout_ms = 500,
+			-- 	lsp_format = "fallback",
+			-- },
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "ruff_organize_imports", "ruff_format" },
