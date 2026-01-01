86a6aa45db1c800dd60155281f127fa77cb92fd7
Ju5t1nL3 <justinle2006@gmail.com>
2025-12-31 22:26:49 -0600

    Make ctrl-d + ctrl-u center and prevent it from change
    Sometimes I accidentally click 4 then ctrl-u, which changes how much
    ctrl-u and ctrl-d go up and down. Now, this prevents that from
    happening.

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -15,3 +15,15 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Override Ctrl-d to always scroll half page, ignoring any numbers (counts) typed before
+vim.keymap.set("n", "<C-d>", function()
+	vim.cmd("normal! \x04") -- \x04 is the internal code for Ctrl-d
+	vim.cmd("normal! zz")
+end, { desc = "Scroll down half page (ignore count)" })
+
+-- Override Ctrl-u to always scroll half page, ignoring any numbers (counts) typed before
+vim.keymap.set("n", "<C-u>", function()
+	vim.cmd("normal! \x15") -- \x15 is the internal code for Ctrl-u
+	vim.cmd("normal! zz")
+end, { desc = "Scroll up half page (ignore count)" })
