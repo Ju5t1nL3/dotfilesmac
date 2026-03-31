e755b46122e343d4c852ce57f094629b37379216
Ju5t1nL3 <justinle2006@gmail.com>
2026-03-31 18:26:53 -0500

    Add haskell shell functionality

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -47,3 +47,5 @@ if status is-interactive
 	starship init fish | source
 	enable_transience
 end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /Users/justinle/.ghcup/bin $PATH # ghcup-env
