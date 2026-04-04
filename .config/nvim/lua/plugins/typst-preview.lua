bcecac4b3db003de6d0b788e7f9d54cf34953947
Ju5t1nL3 <justinle2006@gmail.com>
2026-04-04 16:47:43 -0500

    Only load on typst file types

--- .config/nvim/lua/plugins/typst-preview.lua
+++ .config/nvim/lua/plugins/typst-preview.lua
@@ -1,6 +1,6 @@
 return {
 	"chomosuke/typst-preview.nvim",
-	lazy = false, -- Or ft = 'typst' to only load on typst files
+	ft = 'typst',
 	version = "1.*",
 	build = function()
 		require("typst-preview").update()
