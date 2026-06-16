9bdd78569803585402af59a13ddd8c2cb2fe049f
Justin Le <justinle2006@gmail.com>
2026-06-15 20:09:06 -0700

    Only apply prettier when the prettier package is installed in project

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -11,21 +11,57 @@ return { -- Autoformat
 			desc = "[F]ormat buffer",
 		},
 	},
-	opts = {
-		notify_on_error = false,
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "ruff_organize_imports", "ruff_format" },
-			c = { "clang-format" },
-			cpp = { "clang-format" },
-			haskell = { "fourmolu", stop_after_first = true  },
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-			typescript = { "prettierd", "prettier", stop_after_first = true },
-			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-			css = { "prettierd", "prettier", stop_after_first = true },
-			svg = { "prettierd", "prettier", stop_after_first = true },
-			typst = { "typstyle" },
-		},
-	},
+	opts = function()
+		local function has_config(ctx, names)
+			return vim.fs.find(names, {
+				path = ctx.filename,
+				upward = true,
+			})[1] ~= nil
+		end
+		local function has_prettier(ctx)
+			return has_config(ctx, {
+				".prettierrc",
+				".prettierrc.json",
+				".prettierrc.json5",
+				".prettierrc.yaml",
+				".prettierrc.yml",
+				".prettierrc.js",
+				".prettierrc.cjs",
+				".prettierrc.mjs",
+				".prettierrc.toml",
+				"prettier.config.js",
+				"prettier.config.cjs",
+				"prettier.config.mjs",
+				"prettier.config.ts",
+				"prettier.config.mts",
+				"prettier.config.cts",
+			})
+		end
+
+		return {
+			notify_on_error = false,
+			formatters = {
+				prettier = {
+					condition = has_prettier,
+				},
+				prettierd = {
+					condition = has_prettier,
+				},
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "ruff_organize_imports", "ruff_format" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+				haskell = { "fourmolu", stop_after_first = true },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+				css = { "prettierd", "prettier", stop_after_first = true },
+				svg = { "prettierd", "prettier", stop_after_first = true },
+				typst = { "typstyle" },
+			},
+		}
+	end,
 }
